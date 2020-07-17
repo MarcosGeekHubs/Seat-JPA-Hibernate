aff9ec5f2b94ce22ed7a68a4e001a4a36847b0f5
MarcosW10 <m.palacios@geekshubs.com>
2020-07-17 19:02:48 +0200

    Creamos la anotaciones JPA clase Empleado

--- src/main/java/com/sear/hibernate/modelo/Empleado.java
+++ src/main/java/com/sear/hibernate/modelo/Empleado.java
@@ -1,15 +1,30 @@
 package com.sear.hibernate.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
 
+//Utilizar el de JPA
+@Entity
+@Table(name = "EMPLEADO")
 public class Empleado implements Serializable {
 
     private static final long  serialVersionUID = 1;
 
+    @Id
+    @Column(name = "COD_EMPLEADO")
     private Long codigo;
+
+    @Column(name = "APELLIDOS")
     private String apellidos;
+
+    @Column(name = "NOMBRE")
     private String nombre;
+
+    @Column(name = "FECHA_NACIMIENTO")
     private Date fechaNacimiento;
 
     public Empleado() {
