23a445b93fcbfdafa97e8da460db5591ff676d23
MarcosW10 <m.palacios@geekshubs.com>
2020-07-17 16:24:05 +0200

    Create persistence.xml

--- src/main/java/org/formacion/main/Main.java
+++ src/main/java/org/formacion/main/Main.java
@@ -7,24 +7,7 @@ import javax.persistence.Persistence;
 public class Main {
     public static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_main");
 
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-
-       // Persona nueva = new Persona();
-        // nueva.setNombre("pepe");
-
-       // em.persist(nueva);
-
-        em.getTransaction().commit();
-
-        em.close();
-
-        // Cerramos el entity manager factory para provocar que se cierre el pool
-        // de conexiones que crea Hibernate y no deje la apliacion sin finalizar.
-        emf.close();
 
     }
 }
